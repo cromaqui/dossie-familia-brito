b0b9b49eda6940bff9d923af42394902e6b42c6c
Christopher Augusto <christopher@cromaqui.com>
2025-01-28 10:30:52 -0300

    feat: #10 add share links to social media

--- src/_data/utilities.js
+++ src/_data/utilities.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
 moment.locale('pt');
+require('dotenv').config();
 
 const sentenceCase = function (str) {
   if (typeof str !== 'string' || !str.length) {
@@ -9,9 +10,26 @@ const sentenceCase = function (str) {
   return str[0].toUpperCase() + str.slice(1).toLowerCase();
 };
 
-const humanizeDate = function (date) {
-  return moment(date).format('LL');
+const humanizeDate = function (date, format = 'LL') {
+  return moment(date).format(format);
 };
+
+const shareEntryOnSocialMedia = function (socialMedia, entry) {
+  const { date, id, title } = entry;
+  const url = `${process.env.BASE_URL}/%23${id}`;
+  const text = `"${title}" (${humanizeDate(date, 'DD/MM/YYYY')})%20Relembre isso em: ${url}`;
+  
+  switch (socialMedia) {
+    case 'bluesky':
+      return `https://bsky.app/intent/compose?text=${text}`;
+    case 'facebook':
+      return `https://www.facebook.com/sharer/sharer.php?u=${url}&t=${text}`;
+    case 'x':
+      return `https://x.com/intent/tweet?text=${text}`;
+    default:
+      return "#";
+  }  
+}
 
 const isWrappedInParagraphTags = function (html) {
   if (typeof html !== 'string') {
@@ -24,4 +42,5 @@ module.exports = {
   sentenceCase,
   humanizeDate,
   isWrappedInParagraphTags,
+  shareEntryOnSocialMedia
 };
